ab502cf6ae7b9ab8f2d71fd31c0177cc0b3ed2b4
PrathameshDesai <prathu105143@gmail.com>
2024-06-04 12:15:35 +0530

    java file

---
+++ ABC.java
@@ -0,0 +1,12 @@
+import myinstitute.Department;
+
+class ABC
+{
+public static void main(String args[])
+{
+Department d=new Department();
+d.display("co");
+d.display("if");
+d.display("me");
+}
+}
